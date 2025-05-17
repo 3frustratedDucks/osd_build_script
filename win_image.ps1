523bc10ed0281e19557238fb586911b2cdf43302
Michael Devlin <Mich@elDevlin.com>
2025-05-17 07:19:31 +0100

    Cleaned a lot of the window dressing from the script, I dont need it for this small build script.

--- win_image.ps1
+++ win_image.ps1
@@ -1,28 +1,19 @@
-# Override Global Variables
-
-#Variables to define the Windows OS / Edition to be applied during OSDCloud
-#$OSName = 'Windows 11 23H2 x64' < commented out 23/08/2024 as we may need W10 in niche cases
-$OSEdition = 'Enterprise'
-$OSActivation = 'Retail'
-$OSLanguage = 'en-gb'
-$usbDrive = Get-Volume | Where-Object {$_.DriveType -eq 'Removable'} | Select-Object -First 1 -ExpandProperty DriveLetter
-$osdCloudLocalPath = "$usbDrive"+":\OSDCloud"
-
-#Set OSDCloud Vars
+# Set OSDCloud Vars
 $Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$False
-    RecoveryPartition = [bool]$true
-    OEMActivation = [bool]$True
-    WindowsUpdate = [bool]$true
-    WindowsUpdateDrivers = [bool]$true
-    WindowsDefenderUpdate = [bool]$true
-    SetTimeZone = [bool]$true
-    ClearDiskConfirm = [bool]$False
-    ShutdownSetupComplete = [bool]$false
-    SyncMSUpCatDriverUSB = [bool]$true
-    CheckSHA1 = [bool]$true
+    Restart = $false
+    RecoveryPartition = $true
+    OEMActivation = $true
+    WindowsUpdate = $true
+    WindowsUpdateDrivers = $true
+    WindowsDefenderUpdate = $true
+    SetTimeZone = $true
+    ClearDiskConfirm = $false
+    ShutdownSetupComplete = $false
+    SyncMSUpCatDriverUSB = $true
+    CheckSHA1 = $true
 }
 
+# Simple ASCII banner
 $banner = @"
 //////////////////////////////////////////////////
 //  ___  ____  ____   ____ _                 _  //
@@ -32,31 +23,13 @@ $banner = @"
 // \___/|____/|____/ \____|_|\___/ \__,_|\__,_| //
 //                                              //
 //////////////////////////////////////////////////
-
-// This is a bespoke version of the script
 "@
 $loadingColor = "Yellow"
 Write-Host $banner -ForegroundColor $loadingColor
 
-$loadingMessage1 = "Original created by Dave Segura (@SeguraOSD)"
-$loadingColor = "White"
-$delay = 30  # Delay between each character in milliseconds
-
-# Function to simulate typing effect
-function Type-Write ($message, $color, $delay) {
-    foreach ($char in $message.ToCharArray()) {
-        Write-Host -NoNewline $char -ForegroundColor $color
-        Start-Sleep -Milliseconds $delay
-    }
-    Write-Host  # Move to the next line after the message is fully typed
-}
-
-# Type out the messages with a delay
 Write-Host ""
-Type-Write $loadingMessage1 $loadingColor $delay
-Start-Sleep -Seconds 1  # Optional pause between the lines
-
-write-host ""
+Write-Host "Original created by Dave Segura (@SeguraOSD)" -ForegroundColor White
+Write-Host ""
 
 Write-Host  "Loading OSDCloud..." -ForegroundColor Green
 Write-Host ".................................................." -ForegroundColor Green
